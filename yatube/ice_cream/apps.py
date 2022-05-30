b6efcc65251c10c11fb28a9127a1b701bec0f5f8
Армен Назаров <amnazaroff@gmail.com>
2022-05-30 20:24:47 +0500

    Добавлены urls и view-функции

--- yatube/ice_cream/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IceCreamConfig(AppConfig):
-    name = 'ice_cream'
-    verbose_name: str = 'Управление сортами мороженного'
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def index(request):
+    return HttpResponse('ОНО ЖИВОЕ!!!')
+
+
+def groups_posts(request, slug):
+    return HttpResponse('ДАЖЕ ТУТ ЖИВОЕ')
 # Create your views here.
